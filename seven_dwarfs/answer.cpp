28cd83a2ab988edbe2b5118437ff1dc650677801
dordos <pdhssf@gmail.com>
2022-08-25 14:31:34 -0400

    complete seven_dwarfs

--- seven_dwarfs/answer.cpp
+++ seven_dwarfs/answer.cpp
@@ -3,17 +3,18 @@ using namespace std;
 
 int a[9];
 int main() {
+  // input
   for (int i = 0; i < 9; i++) {
     cin >> a[i];
   }
+
+  // sort the input vlaues
   sort(a, a + 9);
-  for (int i = 0; i < 7; i++) {
-    cout << a[i] << " ";
-  }
-  cout << "\n";
 
   do {
+    // value initialization
     int sum = 0;
+
     for (int i = 0; i < 7; i++) {
       sum += a[i];
     }
@@ -21,6 +22,7 @@ int main() {
     if (sum == 100) {
       break;
     }
+
   } while (next_permutation(a, a + 9));
 
   for (int i = 0; i < 7; i++) {
