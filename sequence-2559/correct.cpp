71d1c79730b86659a885659dac1dd238c7832f22
dordos <pdhssf@gmail.com>
2022-09-02 23:18:20 -0400

    new problemm

---
+++ sequence-2559/correct.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int k, n;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> k;
+
+  cout << k << n
+
+      return 0;
+}
